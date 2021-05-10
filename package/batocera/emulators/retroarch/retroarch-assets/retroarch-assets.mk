d57ee4f428dace1a1269a9c6d40db1774cdd1ce9
James Ou <james1191991@gmail.com>
2021-05-11 01:48:24 +0800

    update(): Add missing assets for retroarch.

--- package/batocera/emulators/retroarch/retroarch-assets/retroarch-assets.mk
+++ package/batocera/emulators/retroarch/retroarch-assets/retroarch-assets.mk
@@ -9,12 +9,16 @@ RETROARCH_ASSETS_SITE = $(call github,libretro,retroarch-assets,$(RETROARCH_ASSE
 RETROARCH_ASSETS_LICENSE = GPL
 
 define RETROARCH_ASSETS_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/share/libretro/assets/xmb
-	cp -r $(@D)/menu_widgets $(TARGET_DIR)/usr/share/libretro/assets
-	cp -r $(@D)/ozone $(TARGET_DIR)/usr/share/libretro/assets
-	cp -r $(@D)/rgui $(TARGET_DIR)/usr/share/libretro/assets
+	cp -r $(@D)/branding  $(TARGET_DIR)/usr/share/libretro/assets
 	cp -r $(@D)/glui $(TARGET_DIR)/usr/share/libretro/assets
-	cp -r $(@D)/xmb/monochrome $(TARGET_DIR)/usr/share/libretro/assets/xmb
+	cp -r $(@D)/menu_widgets $(TARGET_DIR)/usr/share/libretro/assets
+	cp -r $(@D)/nxrgui $(TARGET_DIR)/usr/share/libretro/assets
+	cp -r $(@D)/ozone $(TARGET_DIR)/usr/share/libretro/assets
+	cp -r $(@D)/pkg $(TARGET_DIR)/usr/share/libretro/assets
+	cp -r $(@D)/rgui $(TARGET_DIR)/usr/share/libretro/assets
+	cp -r $(@D)/sounds $(TARGET_DIR)/usr/share/libretro/assets
+	cp -r $(@D)/switch $(TARGET_DIR)/usr/share/libretro/assets
+	cp -r $(@D)/xmb  $(TARGET_DIR)/usr/share/libretro/assets
 endef
 
 $(eval $(generic-package))
