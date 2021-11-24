28265e4e2de2bd17eaa7f45ee8f1c34015dfd885
James Ou <james1191991@gmail.com>
2021-11-25 06:45:33 +0800

    update(es): Bump version.

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_EMULATIONSTATION_VERSION = 76cdc9340899b8b8ff4c770bb6b6bc1cb6e5b8ae
+BATOCERA_EMULATIONSTATION_VERSION = 77f8ad13abbb5b891a3dc1180dcfef94b448474f
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/james1191991/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT
