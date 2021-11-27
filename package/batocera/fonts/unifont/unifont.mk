bdbde862a7fcdbd02f4e91a3cbb99850a14b988f
James Ou <james1191991@gmail.com>
2021-11-27 18:19:14 +0800

    update(): Fix Unifont install script again.

--- package/batocera/fonts/unifont/unifont.mk
+++ package/batocera/fonts/unifont/unifont.mk
@@ -5,12 +5,13 @@
 ################################################################################
 
 UNIFONT_VERSION = 14.0.01
-UNIFONT_SITE = https://unifoundry.com/pub/unifont/unifont-$(UNIFONT_VERSION)/font-builds
-UNIFONT_SOURCE = unifont-$(UNIFONT_VERSION).ttf
+UNIFONT_SITE = https://unifoundry.com/pub/unifont/unifont-$(UNIFONT_VERSION)
+UNIFONT_SOURCE = unifont-$(UNIFONT_VERSION).tar.gz
 UNIFONT_LICENSE = GPL+FE, OFL
 
 define UNIFONT_INSTALL_TARGET_CMDS
-	@cp $(@D)/unifont-$(UNIFONT_VERSION).ttf $(TARGET_DIR)/usr/share/fonts/truetype/unifont.ttf
+	$(INSTALL) -D -m 0644 $(@D)/unifont-$(UNIFONT_VERSION)/font/precompiled/unifont-$(UNIFONT_VERSION).ttf \
+        $(TARGET_DIR)/usr/share/fonts/truetype/unifont.ttf
 endef
 
 $(eval $(generic-package))
